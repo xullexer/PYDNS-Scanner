793df88f1cce768ba39d7fc35413d8e6058b8f32
Pink Orca <ali@pinkorca.net>
2026-02-02 17:37:58 +0330

    refactor: improve error handling and apply black formatting

--- hook-rich.py
+++ hook-rich.py
@@ -1,7 +1,8 @@
 """
 PyInstaller hook for Rich library to include all Unicode data files
 """
+
 from PyInstaller.utils.hooks import collect_data_files
 
 # Collect all data files from rich package including unicode data
-datas = collect_data_files('rich', include_py_files=True)
+datas = collect_data_files("rich", include_py_files=True)
